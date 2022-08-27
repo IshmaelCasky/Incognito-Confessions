bd88225562deb3fe3793a9c02e61be06faa3b1d6
ishmael cascabel <cascabel_ishmael@yahoo.com>
2022-08-27 14:25:55 +0800

    testing

--- resources/views/pages/landing.blade.php
+++ resources/views/pages/landing.blade.php
@@ -10,7 +10,7 @@
                     <x-landing.hero />
                 </div>
             </div>
-            <x-landing.confessions :confessions="$confessions" />
+            {{-- <x-landing.confessions :confessions="$confessions" /> --}}
         </div>
     </div>
 </x-layout>
